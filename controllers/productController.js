bc9e678776c9fc5e6869bfa20c74000e30054147
navedatgames <navedatgames@gmail.com>
2024-05-05 23:28:36 +0530

    added searching + sorting + select in query

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,47 @@
 const Product = require("../models/productModel");
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ message: "Get all products" });
+  //req.query for filter data search
+  const { company, name, sort, select } = req.query;
+  const query = {};
+  if (company) {
+    query.company = company;
+  }
+  // advance search to overcome case sensitive
+  if (name) {
+    query.name = { $regex: name, $options: "i" };
+  }
+
+  let apiData = Product.find(query);
+
+  //sorting asc or desc
+  if (sort) {
+    let sortFix = sort.replace(",", " ");
+    apiData = apiData.sort(sortFix);
+  }
+
+  //select only some field
+  if (select) {
+    let selectedField = select.replace(",", " ");
+    apiData = apiData.select(selectedField);
+  }
+
+  // sort by price default asc if - then des
+  const products = await apiData;
+  res.status(200).json({ data: products });
+};
+
+const getSingleProduct = async (req, res) => {
+  console.log(req.params.id);
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ message: "product not found" });
+    }
+    res.status(200).json({ data: product });
+  } catch (err) {
+    res.status(404).json({ message: "product not found" });
+  }
 };
 
 const createProduct = async (req, res) => {
@@ -12,4 +52,5 @@ const createProduct = async (req, res) => {
 module.exports = {
   getAllProducts,
   createProduct,
+  getSingleProduct,
 };
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { getAllProducts, createProduct } = require("../controllers/productController");
+const {
+  getAllProducts,
+  createProduct,
+  getSingleProduct,
+} = require("../controllers/productController");
 
 const router = express.Router();
 
 router.route("/").get(getAllProducts);
+router.route("/:id").get(getSingleProduct);
 router.route("/create").post(createProduct);
 
 module.exports = router;
