0c07b315569707197ade1627947a6f6ba61e44cf
navedatgames <navedatgames@gmail.com>
2024-05-11 15:32:57 +0530

    added pagination

--- controllers/productController.js
+++ controllers/productController.js
@@ -31,8 +31,16 @@ const getAllProducts = async (req, res) => {
   res.status(200).json({ data: products });
 };
 
+const getAllProductPagination = async (req, res) => {
+  let page = Number(req.query.page) || 1;
+  let limit = Number(req.query.limit) || 2;
+  let skip = (page - 1) * limit;
+
+  const product = await Product.find().skip(skip).limit(limit);
+  res.status(200).json({ data: product, length: product.length });
+};
+
 const getSingleProduct = async (req, res) => {
-  console.log(req.params.id);
   try {
     const product = await Product.findById(req.params.id);
     if (!product) {
@@ -53,4 +61,5 @@ module.exports = {
   getAllProducts,
   createProduct,
   getSingleProduct,
+  getAllProductPagination,
 };
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -3,11 +3,13 @@ const {
   getAllProducts,
   createProduct,
   getSingleProduct,
+  getAllProductPagination,
 } = require("../controllers/productController");
 
 const router = express.Router();
 
 router.route("/").get(getAllProducts);
+router.route("/pagination").get(getAllProductPagination);
 router.route("/:id").get(getSingleProduct);
 router.route("/create").post(createProduct);
 
